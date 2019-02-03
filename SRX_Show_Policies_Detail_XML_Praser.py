2bd7311d62816775d2a8c30dcd11ff700cccb959
Lee Dowdells <lee-2098@live.co.uk>
2019-02-03 12:41:53 +0000

    Fixing isseue with source table, adding seq IDs to printouts. Need to fix dst apps

--- SRX_Show_Policies_Detail_XML_Praser.py
+++ SRX_Show_Policies_Detail_XML_Praser.py
@@ -25,41 +25,41 @@ for POLICY in POLICY_LIST[:]:
   POLICY_SEQ = POLICY['policy-information']['policy-sequence-number']
   POLICY_SRC_ZONE = POLICY['policy-information']['context-information']['source-zone-name']
   POLICY_DST_ZONE = POLICY['policy-information']['context-information']['destination-zone-name']
-  POLICY_SRC_ADD= POLICY['policy-information']['source-addresses']['source-address']
-  POLICY_DST_ADD = POLICY['policy-information']['destination-addresses']['destination-address']
-  POLICY_DST_APP_ADD = POLICY['policy-information']['applications']['application']['application-term']
+  #POLICY_DST_APP_ADD = POLICY['policy-information']['applications']['application']['application-term']
+  #POLICY_DST_APP_TERM = POLICY['policy-information']['applications']['application']
   #
   #Printing Results
   #
-  print("Policy Name: ", POLICY_NAME)
-  print("Policy Action: ", POLICY_ACTION)
-  print("Policy State: ", POLICY_STATE)
-  print("Policy Seq: ", POLICY_SEQ)
-  print("Policy SRC Zone: ", POLICY_SRC_ZONE)
+  print("[",POLICY_SEQ,"]:", "Policy Name: ", POLICY_NAME)
+  print("[",POLICY_SEQ,"]:", "Policy Action: ", POLICY_ACTION)
+  print("[",POLICY_SEQ,"]:", "Policy State: ", POLICY_STATE)
+  print("[",POLICY_SEQ,"]:", "Policy Seq: ", POLICY_SEQ)
+  print("[",POLICY_SEQ,"]:", "Policy SRC Zone: ", POLICY_SRC_ZONE)
+  print("[",POLICY_SEQ,"]:", "Policy DST Zone: ", POLICY_DST_ZONE)
   #
   #Checking if source addresses is a list and if so creating it
   #
+  POLICY_SRC_ADD = POLICY['policy-information']['source-addresses']['source-address']
   POLICY_SRC_ADDRESS_PREFIX_LIST = []
   if isinstance(POLICY_SRC_ADD, list):
     for ADDRESS_PREFIX in POLICY_SRC_ADD[:]:
       POLICY_SRC_ADDRESS_PREFIX_LIST.append(ADDRESS_PREFIX['prefixes']['address-prefix'])
-    print("Policy Sources: ", POLICY_SRC_ADDRESS_PREFIX_LIST)
+    print("[",POLICY['policy-information']['policy-sequence-number'],"]", ": Policy Sources: ", POLICY_SRC_ADDRESS_PREFIX_LIST)
   elif not isinstance(POLICY_SRC_ADD, list):
     POLICY_SRC_ADDRESS_PREFIX_LIST.append(POLICY_SRC_ADD['prefixes']['address-prefix'])
-    print("Policy Sources: ", POLICY_SRC_ADDRESS_PREFIX_LIST)
-  #
-  print("Policy Dst Zone: ", POLICY_DST_ZONE)
+    print("[",POLICY['policy-information']['policy-sequence-number'],"]", ": Policy Sources: ", POLICY_SRC_ADDRESS_PREFIX_LIST)
   #
   #Checking if destination addresses is a list and if so creating it
   #
+  POLICY_DST_ADD = POLICY['policy-information']['destination-addresses']['destination-address']
   POLICY_DST_ADDRESS_PREFIX_LIST = []
   if isinstance(POLICY_DST_ADD, list):
     for ADDRESS_PREFIX in POLICY_DST_ADD[:]:
       POLICY_DST_ADDRESS_PREFIX_LIST.append(ADDRESS_PREFIX['prefixes']['address-prefix'])
-    print("Policy Destinations: ", POLICY_DST_ADDRESS_PREFIX_LIST)
+    print("[",POLICY['policy-information']['policy-sequence-number'],"]""Policy Destinations: ", POLICY_DST_ADDRESS_PREFIX_LIST)
   elif not isinstance(POLICY_DST_ADD, list):
     POLICY_DST_ADDRESS_PREFIX_LIST.append(POLICY_DST_ADD['prefixes']['address-prefix'])
-    print("Policy Destinations: ", POLICY_DST_ADDRESS_PREFIX_LIST)
+    print("[",POLICY['policy-information']['policy-sequence-number'],"]", "Policy Destinations: ", POLICY_DST_ADDRESS_PREFIX_LIST)
   #
   #Checking destination ports and storing
   #  
